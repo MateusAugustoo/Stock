a6bb35bc0dedd389927613885849f0f6eed5f04e
= <=>
2024-09-13 14:10:07 -0300

    refactor: type required inputProps

--- front-end/src/components/InputC.tsx
+++ front-end/src/components/InputC.tsx
@@ -3,19 +3,19 @@ import { useForm } from 'react-hook-form'
 type InputProps = {
   label: string
   type: string
+  required?: boolean
 }
 
 
-export function InputComponent({label, type}: InputProps) {
+export function InputComponent({label, type, required}: InputProps) {
   const { register } = useForm()
 
   return (
     <div>
       <label className='text-sm font-medium'>{label}</label>
       <input
-        required
         type={type} 
-        {...register(label)}
+        {...register(label, {required})}
         className='bg-white w-full h-[3.125rem] pl-2 border border-black rounded-lg' 
       />
     </div>
