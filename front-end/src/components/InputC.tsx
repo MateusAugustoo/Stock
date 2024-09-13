8e3a3122c33053986c4e121627005fd753bc7b01
= <=>
2024-09-13 13:35:05 -0300

    feat: create component Input

---
+++ front-end/src/components/InputC.tsx
@@ -0,0 +1,22 @@
+import { useForm } from 'react-hook-form'
+
+type InputProps = {
+  label: string
+  type: string
+}
+
+
+export function InputComponent({label, type}: InputProps) {
+  const { register } = useForm()
+
+  return (
+    <div>
+      <label className='text-sm font-medium'>{label}</label>
+      <input 
+        type={type} 
+        {...register(label)}
+        className='bg-white w-full h-[3.125rem] pl-2 border border-black rounded-lg' 
+      />
+    </div>
+  )
+}
