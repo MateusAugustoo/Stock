e828c76dce5b8a2ea1d735354f972e942a71902d
= <=>
2024-09-20 00:19:08 -0300

    feat: started project back-end new stack

---
+++ back-end/src/index.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+const start = async () => {
+  try {
+    await app.listen({ port: 3000 });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+start();
