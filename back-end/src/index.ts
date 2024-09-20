66b5a581eaf03b138d79feea9c88b92b43c9eef7
= <=>
2024-09-20 12:22:45 -0300

    refectory: config cors fastify

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,8 +1,19 @@
 import fastify from "fastify";
 import { productRoutes } from "./routes/productRoutes";
+import cors from "@fastify/cors";
 
 const app = fastify({logger: true});
 
+app.register(cors, {
+  origin: (origin, cb) => {
+    if(!origin || origin.includes('localhost')){
+      cb(null, true)
+      return
+    }
+    cb(new Error('Not allowed'), false)
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+})
 app.register(productRoutes)
 
 const start = async () => {
