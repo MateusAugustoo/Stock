c1cb491099ba3b55d1ec43f9a690339745291108
= <=>
2024-09-20 10:03:51 -0300

    feat: create service product

---
+++ back-end/src/service/serviceProduct.ts
@@ -0,0 +1,10 @@
+import { prisma } from "../prisma";
+import { TData } from "../types/TData";
+
+export const createProduct = async (data: TData) => {
+  if(data.name.length > 90){
+    throw new Error('Name exceed the character limit')
+  }
+
+  return await prisma.product.create({data})
+};
