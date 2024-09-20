be7ce385ee3e63635bdcb4aeb4b182765ff45d56
= <=>
2024-09-20 12:18:30 -0300

    refectory: add name and step

--- front-end/src/components/InputC.tsx
+++ front-end/src/components/InputC.tsx
@@ -1,21 +1,24 @@
 import { FieldValues, Path, UseFormRegister } from 'react-hook-form'
 
 type InputProps<T extends FieldValues> = {
-  label: Path<T>
+  label: string
+  name: Path<T>
   type: string
   register: UseFormRegister<T>
   required?: boolean
+  step?: string
 }
 
 
-export function InputComponent<T extends FieldValues>({label,type, register, required}: InputProps<T>) {
+export function InputComponent<T extends FieldValues>({label, name, type, register, required, step}: InputProps<T>) {
   return (
     <div>
-      <label className='text-sm font-medium' htmlFor={label}>{label}</label>
+      <label className='text-sm font-medium' htmlFor={name}>{label}</label>
       <input
-        id={label}
+        id={name}
         type={type}
-        {...register(label, {required})}
+        step={step}
+        {...register(name, {required})}
         className='bg-white w-full h-[3.125rem] pl-2 border border-black rounded-lg' 
       />
     </div>
