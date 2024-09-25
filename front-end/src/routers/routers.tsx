cf9eee89f5813b57bbbe8d4b0788c1e12d37dd83
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-09-24 23:51:11 -0300

    feat: new router products

--- front-end/src/routers/routers.tsx
+++ front-end/src/routers/routers.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import { RegisterProduct } from "../pages/RegisterProduct";
+import { Products } from "../pages/Products";
 
 export const router = createBrowserRouter([
   {
     path: 'register_product',
     element: <RegisterProduct />
+  },
+  {
+    path: 'products',
+    element: <Products />
   }
 ])
