f116449f6fdd875af8f1d3b836db62b576d7432e
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-10-02 00:14:49 -0300

    feat: create folder utils

---
+++ back-end/src/utils/jwt/generatorJWT.ts
@@ -0,0 +1,13 @@
+import jwt, { SignOptions } from 'jsonwebtoken'
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const JWT_SECRET = process.env.JWT_SECRET
+
+export async function generateJWT(payload: object, signOptions: SignOptions = {}){
+  return jwt.sign(payload, JWT_SECRET!, {
+    expiresIn: '1h',
+    ...signOptions
+  })
+}
