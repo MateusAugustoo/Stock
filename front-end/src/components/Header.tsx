573e3a06ed05344da6259f6095782ea50057d19f
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-10-02 22:37:22 -0300

    feat: create component Header default

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,18 +1,19 @@
-import { CircleUserRound } from "lucide-react"
-import { ButtonBack } from "./ButtonBack"
+import { ChevronLeftIcon } from "lucide-react";
 
+type Props = {
+  title: string
+}
 
-export function HeaderComponent() {
+export function HeaderComponent({title}: Props) {
   return (
-    <div className="pt-7">
-      <div className="flex justify-between ">
-        <ButtonBack />
-        <CircleUserRound size={46} />
+    <header className="flex justify-between items-center pt-7">
+      <div className="bg-white border border-black size-10 rounded-lg flex items-center justify-center">
+        <ChevronLeftIcon />
       </div>
-
-      <div className="border border-black mt-7"/>
-
-      <h1 className="text-2xl font-bold text-center mt-5">Cadastre sue Produto</h1>
-    </div>
+      <h2 className="text-2xl font-bold">
+        {title}
+      </h2>
+      <div></div>
+    </header>
   )
 }
