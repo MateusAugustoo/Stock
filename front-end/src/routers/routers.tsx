c69aa75c4c8db2351e16b2b3ff0dde7407132bc3
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-10-03 02:31:17 -0300

    feat: add route register user

--- front-end/src/routers/routers.tsx
+++ front-end/src/routers/routers.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { RegisterProduct } from "../pages/RegisterProduct";
 import { Products } from "../pages/Products";
+import { RegisterUserPage } from "../pages/user/RegisterPage";
 
 export const router = createBrowserRouter([
   {
@@ -10,5 +11,9 @@ export const router = createBrowserRouter([
   {
     path: 'products',
     element: <Products />
+  },
+  {
+    path: '/register_user',
+    element: <RegisterUserPage /> 
   }
 ])
