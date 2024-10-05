703d10e336c470643550473eca30f53ac5906c86
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-10-05 02:27:26 -0300

    feat: update routing to include authentication and protected routes

--- front-end/src/routers/routers.tsx
+++ front-end/src/routers/routers.tsx
@@ -2,18 +2,24 @@ import { createBrowserRouter } from "react-router-dom";
 import { RegisterProduct } from "../pages/RegisterProduct";
 import { Products } from "../pages/Products";
 import { RegisterUserPage } from "../pages/user/RegisterPage";
+import { LoginPage } from "../pages/user/LoginPage";
+import { ProtectedRoute } from "../components/ProtectedRoute";
 
 export const router = createBrowserRouter([
   {
+    path: '/login',
+    element: <LoginPage />
+  },
+  {
+    path: '/register',
+    element: <RegisterUserPage /> 
+  },
+  {
     path: 'register_product',
-    element: <RegisterProduct />
+    element: <ProtectedRoute element={<RegisterProduct />} />
   },
   {
     path: 'products',
-    element: <Products />
-  },
-  {
-    path: '/register_user',
-    element: <RegisterUserPage /> 
+    element: <ProtectedRoute element={<Products />} />
   }
 ])
