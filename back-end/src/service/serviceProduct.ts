c1745cb15e813a02837b376a86060ea6212cfbf7
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-10-09 02:53:11 -0300

    feat: Update createProduct to accept userId parameter.
    Description: The createProduct function now accepts a second parameter, userId, to associate the product with the user who created it.

--- back-end/src/service/serviceProduct.ts
+++ back-end/src/service/serviceProduct.ts
@@ -1,12 +1,17 @@
 import { prisma } from "../prisma";
 import { TData } from "../types/TData";
 
-export const createProduct = async (data: TData) => {
+export const createProduct = async (data: TData, userId: string) => {
   if(data.name.length > 90){
     throw new Error('Name exceed the character limit')
   }
 
-  return await prisma.product.create({data})
+  return await prisma.product.create({
+    data: {
+      ...data,
+      userId
+    }
+  })
 };
 
 export const getProducts = async () => {
