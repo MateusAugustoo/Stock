ec6ec7603488375b28496a1d592ced71c462081d
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-10-09 02:51:21 -0300

    feat: Modify product creation to associate with authenticated user.
    Description: The createProduct function now accepts a second parameter, userId, to associate the product with the user who created it.

--- back-end/src/routes/productRoutes.ts
+++ back-end/src/routes/productRoutes.ts
@@ -1,15 +1,25 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { createProduct, getProducts } from "../service/serviceProduct";
 import { TData } from "../types/TData";
+import { verifyJWT } from "../utils/jwt/verifyJWT";
+import { JwtPayload } from "jsonwebtoken";
 
 export async function productRoutes(fastify: FastifyInstance) {
   fastify.post(
     "/register_products",
+    { preHandler: [verifyJWT] },
     async (request: FastifyRequest, reply: FastifyReply) => {
       try {
         const data = request.body as TData;
-        const product = await createProduct(data);
-        reply.code(201).send(product);
+
+        if (typeof request.user === "object" && "userId" in request.user) {
+          const userId = request.user.userId
+
+          const product = await createProduct(data, userId);
+          reply.code(201).send(product);
+        } else {
+          reply.status(400).send({ message: "Usuário não autenticado" });
+        }
       } catch (error) {
         fastify.log.error(error);
         reply.status(500).send("Internal Server Error");
@@ -17,10 +27,14 @@ export async function productRoutes(fastify: FastifyInstance) {
     }
   );
 
-  fastify.get('/get_products', async (request: FastifyRequest, reply: FastifyReply) => {
-    try {
-      const products = await getProducts()
-      reply.status(200).send(products)
-    } catch(error){}
-  })
+  fastify.get(
+    "/get_products",
+    { preHandler: [verifyJWT] },
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      try {
+        const products = await getProducts();
+        reply.status(200).send(products);
+      } catch (error) {}
+    }
+  );
 }
