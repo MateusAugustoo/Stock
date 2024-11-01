0d3ba22c30183bc8a069db30829517806fe6ede1
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-11-01 03:34:31 -0300

    add attribute image in typeFromProduct in rename

---
+++ front-end/src/types/TProductData.ts
@@ -0,0 +1,10 @@
+export type TProductData = {
+  name: string
+  code: number;
+  expirationDate: Date;
+  category: string;
+  quantity: number;
+  price: number;
+  description: string;
+  image?: string
+}
